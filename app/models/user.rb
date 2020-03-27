9f4279b2e356b20a5f733ff89ce9bc5272d6ee5c
marioa98 <mmartinez53@ucol.mx>
2020-03-27 13:20:05 -0600

    [ADD] Table with email field and validations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :full_name, :username, :password_digest, presence: true
-    validates :username, uniqueness: true
-    has_many :stuffs
+  has_secure_password
+  validates :full_name, :username, :password_digest, :email,presence: true
+  validates :username, uniqueness: true
+  validates :email, format:{with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
+
+  has_many :stuffs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_154621) do
+ActiveRecord::Schema.define(version: 2020_03_27_180143) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_154621) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "noemail@stuff.com", null: false
   end
 
 end
