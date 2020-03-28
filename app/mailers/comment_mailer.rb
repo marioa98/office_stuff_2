6ec6d70e20a1d8d0f0d7a9a12e7ec2db4ab3a03b
marioa98 <mmartinez53@ucol.mx>
2020-03-28 17:21:05 -0600

    [ADD] Mailer for new comment in same publication that I comment

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,14 +1,17 @@
 class CommentMailer < ApplicationMailer
-  
-  def initialize
-    @url = "http://localhost:3000"
-  end
-
   def new_comment
     @stuff = params[:stuff]
     @user = @stuff.user
     @comment = params[:content]
-    @url += "/comments/#{@stuff.id}"
-    mail(to: @user.email, subject: "New comment in '#{@stuff.stuff_name}'")
+    @url = "http://localhost:3000/comments/#{@stuff.id}"
+    mail(to: @user.email, subject: "Your stuff '#{@stuff.stuff_name}', has been commented")
+  end
+
+  def same_stuff_commented
+    @stuff = params[:stuff]
+    recipients = params[:recipients]
+    @url = "http://localhost:3000/comments/#{@stuff.id}"
+    @comment = params[:content]
+    mail(to: recipients, subject: "New comment in stuff '#{@stuff.stuff_name}'") if recipients.any?
   end
 end
