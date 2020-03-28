ca7e13b8a2a2ecb07aa96a00f43b29992a0e337d
marioa98 <mmartinez53@ucol.mx>
2020-03-28 14:17:21 -0600

    [ADD] Notify all admin users when new stuff requested

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,10 +1,14 @@
 class CommentMailer < ApplicationMailer
   
+  def initialize
+    @url = "http://localhost:3000"
+  end
+
   def new_comment
     @stuff = params[:stuff]
     @user = @stuff.user
     @comment = params[:content]
-    @url = "http://localhost:3000/comments/#{@stuff.id}"
+    @url += "/comments/#{@stuff.id}"
     mail(to: @user.email, subject: "New comment in '#{@stuff.stuff_name}'")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default charset: "utf-8"
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
@@ -43,7 +43,7 @@ Rails.application.configure do
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       :login,
-    enable_starttls_auto: true 
+    enable_starttls_auto: true
   }
 
   config.action_mailer.default_url_options = {host: "localhost:3000"}
